7df790790732dcef10ca761886679d1b8afff422
j.myeong_ <jmyeong012@gmail.com>
2022-08-11 17:10:31 +0900

    [SPT-06] feat. Matching 조회

--- src/services/matching-service.ts
+++ src/services/matching-service.ts
@@ -1,9 +1,11 @@
 import { ObjectId } from 'mongoose';
 import { MatchingDocument } from '../entities/matching-entity';
 import { ClientError } from '../errors/base-error';
+import { Matching } from '../models/matching-model';
+import { UserDocument, UserType } from '../entities/user-entity';
 
 export async function createMatching(args: {
-  host: ObjectId,
+  host: string,
   place: {
     name: string,
     address: string,
@@ -21,9 +23,28 @@ export async function createMatching(args: {
     level: number,
   },
   url?: string,
-  isValid: boolean,
   isPublic: boolean,
   thumbnail?: string,
-}): Promise<MatchingDocument> {
-  throw new ClientError('Test', 401);
+}, user: UserDocument | undefined): Promise<MatchingDocument> {
+  if (user) {
+    if ((args.host === String(user._id) && user.isActive) || user.role === UserType.Admin) {
+      const matching = new Matching(args);
+      await matching.save();
+      return matching;
+    }
+    throw new ClientError('권한이 없습니다.', 401);
+  }
+  throw new ClientError('BadParameters', 400);
+}
+
+export async function getAllMatching() {
+  const documents = await Matching.find({}, ['_id', 'title', 'date', 'place', 'approval', 'condition']).lean();
+  const length = await Matching.find().count();
+  const matchings = documents.map((data) => {
+    return { ...data, approval: data.approval.length };
+  });
+  return {
+    length,
+    matchings,
+  };
 }
